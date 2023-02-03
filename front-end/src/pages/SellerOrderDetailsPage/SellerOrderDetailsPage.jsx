f8eb18dd9a7cac801e51c3af4548a5da1090d93a
Erik Yamamoto <erik.ymmt@gmail.com>
2023-02-03 10:34:35 -0300

    feat(css): seller details

--- front-end/src/pages/SellerOrderDetailsPage/SellerOrderDetailsPage.jsx
+++ front-end/src/pages/SellerOrderDetailsPage/SellerOrderDetailsPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Col, Container, Row } from 'reactstrap';
 import NavbarsSeller from '../../components/NavbarSeller';
 import getOneSaleDetails from '../../services/getOneSaleDetails';
 import getSaleProducts from '../../services/getSaleProducts';
@@ -47,55 +48,81 @@ export default function SellerOrderDetailsPage() {
   return (
     <div>
       <NavbarsSeller />
-      <h2>Detalhes do Pedido</h2>
-      <div data-testid="seller_order_details__element-order-details-label-order-id">
-        Pedido
-        {' '}
-        {sale.id}
-      </div>
-      <div data-testid="seller_order_details__element-order-details-label-order-date">
-        {new Date(sale.saleDate).toLocaleDateString('pt-br')}
-      </div>
-      <div
-        data-testid="seller_order_details__element-order-details-label-delivery-status"
-      >
-        {orderStatus}
-      </div>
-      <button
-        type="button"
-        onClick={ prepareOrder }
-        data-testid="seller_order_details__button-preparing-check"
-        disabled={ orderStatus === PREPARANDO || orderStatus === ENTREGUE }
-      >
-        PREPARAR PEDIDO
-      </button>
-      <button
-        type="button"
-        onClick={ deliveryOrder }
-        data-testid="seller_order_details__button-dispatch-check"
-        disabled={
-          orderStatus !== PREPARANDO
-          || orderStatus === EMTRANSITO
-          || orderStatus === ENTREGUE
-        }
-      >
-        SAIU PARA ENTREGA
-      </button>
-      <SaleProductsTable saleProducts={ saleProducts } />
-      <span>
-        Preço Total:
-        {' '}
-      </span>
-      <div data-testid="seller_order_details__element-order-total-price">
-        {String(sale.totalPrice).replace('.', ',')}
-      </div>
-      <div>
-        Endereço:
-        {' '}
-        {sale.deliveryAddress}
-        ,
-        {sale.deliveryNumber}
-      </div>
+      <Container className="d-flex flex-column gap-2 mt-4">
+        <Row>
+          <h3>Detalhes do Pedido</h3>
+        </Row>
+
+        <Row className="d-flex align-items-center border py-3">
+          <Col className="d-flex gap-4">
+            <div
+              data-testid="seller_order_details__element-order-details-label-order-id"
+              className="d-flex align-items-center"
+            >
+              Pedido
+              {' '}
+              {sale.id}
+            </div>
+            <div
+              data-testid="seller_order_details__element-order-details-label-order-date"
+              className="d-flex align-items-center"
+            >
+              {new Date(sale.saleDate).toLocaleDateString('pt-br')}
+            </div>
+            <div
+              data-testid="seller_order_details__element-order-details-label-delivery-status"
+              className={ `${orderStatus === 'Em Trânsito' ? 'EmTransito' : orderStatus}
+              py-1 px-2 rounded fw-bold` }
+            >
+              {orderStatus}
+            </div>
+          </Col>
+          <Col className="d-flex justify-content-end gap-2">
+            <button
+              type="button"
+              onClick={ prepareOrder }
+              data-testid="seller_order_details__button-preparing-check"
+              disabled={ orderStatus === PREPARANDO || orderStatus === ENTREGUE }
+              className="btn btn-secondary"
+            >
+              PREPARAR PEDIDO
+            </button>
+            <button
+              type="button"
+              onClick={ deliveryOrder }
+              data-testid="seller_order_details__button-dispatch-check"
+              disabled={
+                orderStatus !== PREPARANDO
+              || orderStatus === EMTRANSITO
+              || orderStatus === ENTREGUE
+              }
+              className="btn btn-success"
+            >
+              SAIU PARA ENTREGA
+            </button>
+          </Col>
+        </Row>
+
+        <Row>
+          <SaleProductsTable saleProducts={ saleProducts } />
+        </Row>
+        <Row>
+          <span>
+            Preço Total:
+            {' '}
+          </span>
+          <div data-testid="seller_order_details__element-order-total-price">
+            {String(sale.totalPrice).replace('.', ',')}
+          </div>
+          <div>
+            Endereço:
+            {' '}
+            {sale.deliveryAddress}
+            ,
+            {sale.deliveryNumber}
+          </div>
+        </Row>
+      </Container>
     </div>
   );
 }
