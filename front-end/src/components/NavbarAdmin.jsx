6c03ebb42f8174ba3b4da28b7301cd85b020b281
Erik Yamamoto <erik.ymmt@gmail.com>
2023-02-03 09:55:53 -0300

    feat(css): adjust navbar

--- front-end/src/components/NavbarAdmin.jsx
+++ front-end/src/components/NavbarAdmin.jsx
@@ -1,5 +1,6 @@
 import { useHistory } from 'react-router-dom';
 import './styles/NavBarAdmin.css';
+import logo from '../images/silverio_lanches_logo.png';
 
 function NavbarAdmin() {
   const history = useHistory();
@@ -12,29 +13,36 @@ function NavbarAdmin() {
   const user = JSON.parse(localStorage.getItem('user'));
 
   return (
-    <header className="navbarAdminPage d-flex justify-content-between">
-      <span
-        className="text-white font-Roboto fs-4 fw-bold border border-danger"
-        data-testid="customer_products__element-navbar-link-orders"
-      >
-        GERENCIAR USUÁRIOS
-      </span>
+    <header
+      className="navbarAdminPage d-flex shadow-sm
+    justify-content-between align-items-center px-5 py-2"
+    >
+      <div className="d-flex gap-4 align-items-center">
+        <img src={ logo } alt="logo" className="logo" />
+        <span
+          className="text-white font-Roboto fs-5 fw-bold"
+          data-testid="customer_products__element-navbar-link-orders"
+        >
+          GERENCIAR USUÁRIOS
+        </span>
+      </div>
 
-      <span
-        className="text-white font-Roboto fs-4 fw-light ps-2 border border-danger"
-        data-testid="customer_products__element-navbar-user-full-name"
-      >
-        {user?.name}
-      </span>
-
-      <button
-        onClick={ handleOnClickLoggout }
-        type="button"
-        className="btnSair text-white font-Roboto fs-4 fw-light me-2"
-        data-testid="customer_products__element-navbar-link-logout"
-      >
-        Sair
-      </button>
+      <div className="d-flex gap-4 align-items-center">
+        <span
+          className="text-white font-Roboto fs-5 fw-light ps-2"
+          data-testid="customer_products__element-navbar-user-full-name"
+        >
+          {user?.name}
+        </span>
+        <button
+          onClick={ handleOnClickLoggout }
+          type="button"
+          className="btnSair text-white font-Roboto fs-5 fw-light me-2"
+          data-testid="customer_products__element-navbar-link-logout"
+        >
+          Sair
+        </button>
+      </div>
     </header>
   );
 }
