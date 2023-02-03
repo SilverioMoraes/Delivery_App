9eb92249fb2fcea81673c36ea5fa743d267a19a7
Erik Yamamoto <erik.ymmt@gmail.com>
2023-02-03 16:41:53 -0300

    feat(css): customer navbar

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import { Link, useHistory } from 'react-router-dom';
+import './styles/NavBarAdmin.css';
+import logo from '../images/silverio_lanches_logo.png';
 
 export default function Navbar() {
   const history = useHistory();
@@ -10,34 +12,45 @@ export default function Navbar() {
   const user = JSON.parse(localStorage.getItem('user'));
 
   return (
-    <header>
-      <Link
-        to="/customer/products"
-        data-testid="customer_products__element-navbar-link-products"
-      >
-        PRODUTOS
-      </Link>
+    <header
+      className="navbarAdminPage d-flex shadow-sm
+    justify-content-between align-items-center px-5 py-2"
+    >
+      <div className="d-flex gap-4 align-items-center">
+        <img src={ logo } alt="logo" className="logo" />
+        <Link
+          className="text-white font-Roboto fs-5 fw-bold text-decoration-none"
+          to="/customer/products"
+          data-testid="customer_products__element-navbar-link-products"
+        >
+          PRODUTOS
+        </Link>
 
-      <Link
-        to="/customer/orders"
-        data-testid="customer_products__element-navbar-link-orders"
-      >
-        MEUS PEDIDOS
-      </Link>
+        <Link
+          className="text-white font-Roboto fs-5 fw-bold text-decoration-none"
+          to="/customer/orders"
+          data-testid="customer_products__element-navbar-link-orders"
+        >
+          MEUS PEDIDOS
+        </Link>
+      </div>
 
-      <span
-        data-testid="customer_products__element-navbar-user-full-name"
-      >
-        {user?.name}
-      </span>
-
-      <button
-        onClick={ handleOnClickLoggout }
-        type="button"
-        data-testid="customer_products__element-navbar-link-logout"
-      >
-        Sair
-      </button>
+      <div className="d-flex gap-4 align-items-center">
+        <span
+          className="text-white font-Roboto fs-5 fw-bold"
+          data-testid="customer_products__element-navbar-user-full-name"
+        >
+          {user?.name}
+        </span>
+        <button
+          className="btnSair text-white font-Roboto fs-5 fw-light me-2"
+          onClick={ handleOnClickLoggout }
+          type="button"
+          data-testid="customer_products__element-navbar-link-logout"
+        >
+          Sair
+        </button>
+      </div>
     </header>
   );
 }
